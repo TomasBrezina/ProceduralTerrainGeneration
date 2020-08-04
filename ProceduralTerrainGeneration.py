3a3736745422ca14f8da7c932172f26636a11fd3
atom995 <brezina.business@gmail.com>
2020-08-04 18:04:08 +0200

    Changing settings, saving, ...

--- ProceduralTerrainGeneration.py
+++ ProceduralTerrainGeneration.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.filedialog import asksaveasfilename,asksaveasfile
 from PIL.Image import fromarray
 from PIL import ImageTk, Image
 import noise
@@ -19,6 +20,153 @@ WHT = "#eeeeee"
 FONT = ("Tahoma","15")
 FONT2 = ("Tahoma", "8")
 
+
+class Generator:
+    def __init__(self):
+        pass
+
+    def get_random_noise(self, scale, shape, octaves, persistence, lacunarity, seed=False):
+        scale = shape[1] * (scale / 100)
+        persistence /= 100
+        world = np.zeros(shape)
+        if seed == False or seed <= 0: seed = np.random.randint(1,100)
+        for i in range(shape[0]):
+            for j in range(shape[1]):
+                world[i][j] = noise.pnoise2(i / scale, j / scale,
+                                            octaves=octaves,
+                                            persistence=persistence,
+                                            lacunarity=lacunarity,
+                                            base=seed)
+        return ((world + 1) * 128).astype(np.uint8)
+
+    def assign_colors(self, layers, noise_array, sea_level=120):
+        altitudes = (layers[:, 2] + sea_level).astype(int)
+        colors = np.array([np.array([*color], dtype=np.uint8) for color in layers[:, 1]])
+        color_indices = np.digitize(noise_array, altitudes)
+        color_array = np.array([colors[ind] for ind in color_indices])
+        return color_array
+
+    def array_to_image(self,color_world):
+        return fromarray(color_world, mode="RGB")
+
+class App:
+    def __init__(self, master, stg, layers):
+        self.master = master
+        self.stg = stg
+        self.layers = layers
+
+        self.gen = Generator()
+
+        height,width = self.master.winfo_screenheight(),self.master.winfo_screenwidth()
+        # default, min, max
+        self.stg["height"] = [height//2, height//10, height]
+        self.stg["width"] = [height//2, height//10, width]
+
+        self.master.resizable(0,0)
+        self.master.geometry(str(self.stg["width"][0]) + "x" + str(self.stg["height"][0]))
+
+        self.show = True
+
+        self.image = None
+        self.tk_image = None
+        self.bg_label = Label(self.master, background="white", image=self.tk_image)
+
+        self.frm = {}
+        self.lbl = {}
+        self.wid = {}
+
+        self.wid["generate"] = Button(self.master, text="Generate", font=FONT, bg=WHT, activebackground=WHT, relief=SOLID,command=self.generate)
+        self.wid["show"] = Button(self.master, text="Show settings", font=FONT2, bg=WHT, activebackground=WHT, relief=SOLID,command=self.show_frames)
+        self.wid["save"] = Button(self.master, text="Save image", font=FONT2, bg=WHT, activebackground=WHT, relief=SOLID,command=self.save)
+
+
+        for key in self.stg:
+            sub_stg = self.stg[key]
+            if len(sub_stg) == 3:
+                self.frm[key] = Frame(self.master, bg=WHT, relief=SOLID, bd=1)
+                self.lbl[key] = Label(self.frm[key], text=key, font=FONT2, bg=WHT)
+                self.wid[key] = Spinbox(self.frm[key], width=4, from_=sub_stg[1], to=sub_stg[2], bg=WHT, relief=SOLID)
+        self.default_settings()
+
+        self.lbl["seed"].config(text="seed [0=rand]")
+
+        self.draw()
+        self.generate()
+
+    def get_inputs(self):
+        for key in self.frm:
+            value = int(self.wid[key].get())
+            if value < self.stg[key][1]: value = self.stg[key][1] #min
+            elif value > self.stg[key][2]: value = self.stg[key][2]  # min
+            self.stg[key][0] = value
+            self.wid[key].delete(0, "end")
+            self.wid[key].insert(0, self.stg[key][0])
+
+    def default_settings(self):
+        for key in self.frm:
+            self.wid[key].delete(0, "end")
+            self.wid[key].insert(0, self.stg[key][0])
+
+    @timing
+    def generate(self):
+        self.get_inputs()
+
+        noise_array = self.gen.get_random_noise(
+            self.stg["scale"][0], (self.stg["height"][0], self.stg["width"][0]), self.stg["octaves"][0], self.stg["persistence"][0], self.stg["lacunarity"][0],
+            seed=self.stg["seed"][0]
+        )
+        color_array = self.gen.assign_colors(layers, noise_array, 120)
+
+        self.image = self.gen.array_to_image(color_array)
+        self.tk_image = ImageTk.PhotoImage(self.image)
+        self.bg_label.config(image=self.tk_image)
+
+        self.master.geometry(str(self.stg["width"][0]) + "x" + str(self.stg["height"][0]))
+
+
+    def draw(self):
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.wid["generate"].pack(anchor=NE, padx=15, pady=15)
+        self.wid["save"].pack(anchor=NE, padx=15, pady=5)
+        self.wid["show"].pack(anchor=NE, padx=15, pady=5)
+
+        for name in self.frm:
+            self.lbl[name].pack(side=LEFT, padx=2)
+            self.wid[name].pack(side=LEFT, padx=2)
+        self.show_frames()
+
+    def show_frames(self):
+        if self.show:
+            for key in self.frm:
+                self.frm[key].pack(anchor=NE, padx=15, pady=2)
+            self.show = False
+            self.wid["show"].config(text="Hide settings")
+        else:
+            for key in self.frm:
+                self.frm[key].pack_forget()
+            self.show = True
+            self.wid["show"].config(text="Show settings")
+
+    def save(self):
+        dir = asksaveasfilename(
+            title="Save your terrain"
+        )
+        if dir:
+            self.image.save(dir+".png")
+
+# (default, min, max)
+stg = {
+    "height": [500, 0, 0],  # resolution
+    "width": [500, 0, 0], # resolution
+
+    "seed" : [False, 0, 100],
+    "sea_level": [120, 100, 140], # altitude
+    "scale" : [45, 1, 100],
+    "octaves" : [6, 1, 15],
+    "persistence" : [55, 1, 100],
+    "lacunarity" : [2, 1, 10]
+}
+
 # name, color, altitude
 layers = np.array([
     ["blue1", (22, 156, 233), -10],
@@ -34,112 +182,6 @@ layers = np.array([
     ["snow", (245, 240, 240), 255]
 ])
 
-class Generator:
-    def __init__(self):
-        pass
-
-    @timing
-    def get_random_noise(self, scale, shape, octaves, persistence, lacunarity, seed=False):
-        scale = shape[1] * (scale / 100)
-        world = np.zeros(shape)
-        if seed == False or seed <= 0: seed = np.random.randint(1,100)
-        for i in range(shape[0]):
-            for j in range(shape[1]):
-                world[i][j] = noise.pnoise2(i / scale, j / scale,
-                                            octaves=octaves,
-                                            persistence=persistence,
-                                            lacunarity=lacunarity,
-                                            base=seed)
-        return ((world + 1) * 128).astype(np.uint8)
-    @timing
-    def assign_colors(self, layers, noise_array, sea_level=120):
-        altitudes = (layers[:, 2] + sea_level).astype(int)
-        colors = np.array([np.array([*color], dtype=np.uint8) for color in layers[:, 1]])
-        color_indices = np.digitize(noise_array, altitudes)
-        color_array = np.array([colors[ind] for ind in color_indices])
-        return color_array
-    @timing
-    def array_to_image(self,color_world):
-        return fromarray(color_world, mode="RGB")
-
-class App:
-    def __init__(self, master, stg):
-        self.stg = stg
-        self.master = master
-
-        self.stg["shape"] = (self.master.winfo_screenheight()//2,self.master.winfo_screenheight()//2)
-
-        self.master.resizable(0,0)
-        self.master.geometry(str(self.stg["shape"][1]) + "x" + str(str(self.stg["shape"][0])))
-
-        self.gen = Generator()
-
-        self.image = None
-        self.tk_image = None
-        self.bg_label = Label(self.master, background="white", image=self.tk_image)
-
-        self.frm = {}
-        self.lbl = {}
-        self.wid = {}
-
-        self.wid["generate"] = Button(self.master, text="Generate", font=FONT, bg=WHT, activebackground=WHT, relief=SOLID,command=self.generate)
-
-        self.frm["seed"] = Frame(self.master, bg=WHT, relief=SOLID, bd=1)
-        self.lbl["seed"] = Label(self.frm["seed"], text="Seed (0 if random)", font=FONT2, bg=WHT)
-        self.wid["seed"] = Spinbox(self.frm["seed"], width=4, from_=1, to=100, bg=WHT, relief=SOLID)
-
-        self.frm["octaves"] = Frame(self.master, bg=WHT, relief=SOLID, bd=1)
-        self.lbl["octaves"] = Label(self.frm["octaves"], text="Octaves", font=FONT2, bg=WHT)
-        self.wid["octaves"] = Spinbox(self.frm["octaves"], width=4, from_=1, to=15, bg=WHT, relief=SOLID)
-
-        self.frm["scale"] = Frame(self.master, bg=WHT, relief=SOLID, bd=1)
-        self.lbl["scale"] = Label(self.frm["scale"], text="Scale", font=FONT2, bg=WHT)
-        self.wid["scale"] = Spinbox(self.frm["scale"], width=4, from_=1, to=100, bg=WHT, relief=SOLID)
-
-        for name in self.frm:
-            self.wid[name].delete(0,"end")
-            self.wid[name].insert(0,self.stg[name])
-        self.draw()
-        self.generate()
-
-    def get_inputs(self):
-        for name in self.frm:
-            self.stg[name] = int(self.wid[name].get())
-
-    def generate(self):
-        self.get_inputs()
-
-        noise_array = self.gen.get_random_noise(
-            self.stg["scale"], self.stg["shape"], self.stg["octaves"], self.stg["persistence"], self.stg["lacunarity"],
-            seed=self.stg["seed"]
-        )
-        color_array = self.gen.assign_colors(layers, noise_array, 120)
-
-        self.image = self.gen.array_to_image(color_array)
-        self.tk_image = ImageTk.PhotoImage(self.image)
-        self.bg_label.config(image=self.tk_image)
-        #image.show()
-
-    def draw(self):
-        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
-        self.wid["generate"].pack(anchor=NE, padx=15, pady=15)
-
-        for name in self.frm:
-            self.frm[name].pack(anchor=NE,padx=15,pady=5)
-            self.lbl[name].pack(side=LEFT, padx=5)
-            self.wid[name].pack(side=LEFT, padx=5)
-
-
-stg = {
-    "seed" : False,
-    "sea_level": 120, # altitude
-    "shape" : (500, 500), # resolution
-    "scale" : 45,
-    "octaves" : 6,
-    "persistence" : 0.55,
-    "lacunarity" : 2
-}
-
 root = Tk()
-app = App(root, stg)
+app = App(root, stg, layers)
 root.mainloop()
